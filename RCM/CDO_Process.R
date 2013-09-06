15251ac13b4ba5d2f37cee35fcb3a5b8d0459277
user <user@user-pc.(none)>
2013-09-06 12:56:46 -0500

    switching plotting to levelplot instead of spplot

--- RCM/CDO_Process.R
+++ RCM/CDO_Process.R
@@ -1,11 +1,10 @@
 
 ### set the working directory
-setwd("/media/Data/Work/Courses/CyberInfrastructure/ClimateModule/Courses/Day3")
 setwd("/home/user/ClimateLabs/RCM")
 
 ## load some libraries that we will need
 library(ncdf)
-library(raster)
+library(rasterVis)
 library(sp)
 library(rgdal)
 library(reshape)
@@ -15,11 +14,8 @@ library(lattice)
 ## install.packages("ncdf")
 ## and then re-run the lines above to load the newly installed libraries
 
-## look at what files are there
-list.files()
-## do you see the two .nc files you downloaded?  
-## If not, you either did not set the working directory correctly 
-## or did not save the files in the right place.
+## look at what files are in the data directory
+list.files("../data")
 
 ## to run a CDO command from R, we have to 'wrap' it with a "system" call.  
 ## Try this:
@@ -28,17 +24,21 @@ system("cdo -V")
 ## that command tells R to run the command "cdo -V" at the system level.
 ## -V says you want to see what version of CDO you are have installed.
 ## If it worked, you should see a list of the various components of CDO
-## and their version numbers and dates in the R console.  If you don't see this output, let Adam know.
+## and their version numbers and dates in the R console.  If you don't see this output, let us know.
 
 ## Let's take a closer look at one of those files using the "variable description" (VARDES) command:
-system("cdo vardes gfdl_RCM3_Future.nc")
+system("cdo vardes ../data/gfdl_RCM3_Future.nc")
+## note that there is a "../data/" command before the filename. This tells cdo to go up one level
+## in the directory tree, then go into the data directory to find the file.  
+## using relative paths like this allow the top-level folder to be moved
+## without breaking links (even on other computers)
 
 ## What is the output?  What variables are in that file?
 ## You can also explore the file with panoply
 ## adding the "&" on the end tells linux to start panoply and not wait for it to close/finish
 ## But it remains a 'sub' process of R here, so if you close R, the panoply window will also die.
 ## See nohup (http://en.wikipedia.org/wiki/Nohup) for ways to spawn an independant process.
-system("/usr/local/PanoplyJ/panoply.sh gfdl_RCM3_Future.nc &")
+system("/usr/local/PanoplyJ/panoply.sh ../data/gfdl_RCM3_Future.nc &")
 
 ## CDO commands generally have the following syntax:  cdo command inputfile outputfile 
 ## and create a new file (outputfile) that is the result of running the command on the 
@@ -46,9 +46,10 @@ system("/usr/local/PanoplyJ/panoply.sh gfdl_RCM3_Future.nc &")
 
 ## What if you wanted to select only one variable (selvar) out of the netcdf file?  
 ## Try this:
-system("cdo selvar,tmean gfdl_RCM3_Future.nc gfdl_RCM3_Future_tmean.nc")
+system("cdo selvar,tmean ../data/gfdl_RCM3_Future.nc gfdl_RCM3_Future_tmean.nc")
 
-## Now inspect the resulting file (gfdl_RCM3_Future_tmean.nc) with Panoply 
+## Now inspect the resulting file (gfdl_RCM3_Future_tmean.nc) with Panoply
+## (you can open additional files using the panoply menus)
 ## or using the vardes command above.  
 ## What's different about it?  Do you understand what the selvar command does?  
 
@@ -58,7 +59,7 @@ system("cdo selvar,tmean gfdl_RCM3_Future.nc gfdl_RCM3_Future_tmean.nc")
 ## commands will be run in parallel if possible (up to 128 threads).  This comes in 
 ## handy when you are working with hundreds of GB of data.  Now convert the daily data 
 ## to mean annual temperature with the following command:
-system("cdo timmean -selvar,tmean gfdl_RCM3_Future.nc gfdl_RCM3_Future_mat.nc")
+system("cdo timmean -selvar,tmean ../data/gfdl_RCM3_Future.nc gfdl_RCM3_Future_mat.nc")
 
 ## That command first uses selvar to subset only tmean (mean temperatures), 
 ## then uses timmean to take the average over the entire 'future' timeseries.
@@ -67,7 +68,7 @@ system("cdo timmean -selvar,tmean gfdl_RCM3_Future.nc gfdl_RCM3_Future_mat.nc")
 
 ## Now calculate the same thing (mean annual temperature) from the model output for the "Current" 
 ## time period:
-system("cdo timmean -selvar,tmean gfdl_RCM3_Current.nc gfdl_RCM3_Current_mat.nc")
+system("cdo timmean -selvar,tmean ../data/gfdl_RCM3_Current.nc gfdl_RCM3_Current_mat.nc")
 
 ## And calculate the difference between them:
 system("cdo sub gfdl_RCM3_Future_mat.nc gfdl_RCM3_Current_mat.nc gfdl_RCM3_mat_dif.nc")
@@ -77,32 +78,28 @@ system("cdo sub gfdl_RCM3_Future_mat.nc gfdl_RCM3_Current_mat.nc gfdl_RCM3_mat_d
 ## Or you can string the commands above together and do all of the previous 
 ## operations in one step with the following command 
 ## (and if you have a dual core - or more - processor it will use all of them, isn't that cool!):
-system("cdo sub -timmean -selvar,tmean gfdl_RCM3_Future.nc -timmean -selvar,tmean gfdl_RCM3_Current.nc gfdl_RCM3_mat_dif.nc")
+system("cdo sub -timmean -selvar,tmean ../data/gfdl_RCM3_Future.nc -timmean -selvar,tmean ../data/gfdl_RCM3_Current.nc gfdl_RCM3_mat_dif.nc")
 
 ## How does that command differ (in speed and ease) to calculating mean annual temperature like we did it last week? 
 
-##  Now load these summarized data into R as a "SpatialGridDataFrame" 
+##  Now load these summarized data into R as a 'raster' class (defined by the raster library) 
 ## using the raster package which allows you to read directly from a netcdf file.
 ## You could also use the rgdal package if it has netcdf support (which it doesn't by default in windows)
-mat_dif=as(raster("gfdl_RCM3_mat_dif.nc",varname="tmean",band=1),"SpatialGridDataFrame")
-## That command first loads the data as a 'raster' object then converts it to a "SpatialGridDataFrame" 
-## for easy plotting with spplot.
+mat_dif=raster("gfdl_RCM3_mat_dif.nc",varname="tmean",band=1)
 
 ##load a polygon (shapefile) of New England to overlay on the grid so you know what you are looking at
+## the shapefile has polygons, but since we just want this for an overlay, let's convert to lines as well
 ne=as(readOGR("NewEngland.shp","NewEngland"),"SpatialLines")
 
 ## take a look at the data
-spplot(mat_dif,sp.layout=list("sp.lines",ne))
-
-## if you don't like those colors, try some others (and add axis):
-spplot(mat_dif,sp.layout=list("sp.lines",ne),col.regions=rev(heat.colors(20)),
-scales=list(draw=T))
+levelplot(mat_dif,margin=F)+layer(sp.lines(ne))
 
 ## So how much does that model say it will warm by mid-century?  
 ## What patterns do you see at this scale?  An difference between water and land?  Why would this be?
 
 ## The previous plot shows the information over space, but how about a distribution of values:
-hist(mat_dif@data$Mean.Daily.Temperature,col="grey",main="Change (future-current) Mean Annual Temperature",xlab="Change (Degrees C)")
+hist(mat_dif,col="grey",main="Change (future-current) \n Mean Annual Temperature",
+     xlab="Change (Degrees C)",xlim=c(1.3,2.5))
 
 ######################################################################
 ######################################################################
@@ -110,12 +107,12 @@ hist(mat_dif@data$Mean.Daily.Temperature,col="grey",main="Change (future-current
 
 ## We now need to calculate the change in mean annual temp and precipitation in the GCM output.
 ## You will have to edit the file names unless you saved them with exactly these names:
-system("cdo sub -timmean GFDL_Future.nc -timmean GFDL_Current.nc GFDL_dif.nc")
+system("cdo sub -timmean ../data/GFDL_Future.nc -timmean ../data/GFDL_Current.nc GFDL_dif.nc")
 ## you might see some warnings about missing variables, this is due to how IRI writes it's netCDF files
 ## in this example we don't need to worry about them
 
 ## You can also calculate the seasonal mean change from this file like we did above:
-system("cdo sub -yseasmean GFDL_Future.nc -yseasmean GFDL_Current.nc  GFDL_difseas.nc")
+system("cdo sub -yseasmean ../data/GFDL_Future.nc -yseasmean ../data/GFDL_Current.nc  GFDL_difseas.nc")
 
 
 ## To compare the GCM and RCM data and resolutions, 
@@ -125,6 +122,18 @@ system("cdo sub -yseasmean GFDL_Future.nc -yseasmean GFDL_Current.nc  GFDL_difse
 ## How do you think the differences in resolution would affect analysis of biological/anthropological data?
 ## Is the increased resolution useful?
 
+## OR we can plot a comparison via R
+gcm=brick(raster("GFDL_dif.nc",varname="tmean"),raster("GFDL_dif.nc",varname="ptot"))
+rcm=brick(raster("GFDL_RCM3_dif.nc",varname="tmean"),raster("GFDL_RCM3_dif.nc",varname="ptot"))
+
+## it's not possible to merge rasters of different resolutions as a single raster or brick object
+## but you can combine the plots easily with some functions in the rasterVis/latticeExtra packages like this:
+c(levelplot(gcm[[1]],margin=F,main="Projected change in \n mean annual temperature (C)"),
+  levelplot(rcm[[1]],margin=F))+layer(sp.lines(ne))
+
+c(levelplot(gcm[[2]],margin=F,main="Projected change in \n mean annual precipitation (mm)"),
+  levelplot(rcm[[2]],margin=F))+layer(sp.lines(ne))
+
 ######################################################################
 ######################################################################
 ######################################################################
@@ -133,33 +142,29 @@ system("cdo sub -yseasmean GFDL_Future.nc -yseasmean GFDL_Current.nc  GFDL_difse
 
 ## 1) Calculate the mean change (future-current) for all the variables in the GFDL_RCM3 dataset (tmin, tmax, tmean, and precipitation) like we did for just temperature above (Hint: do them one by one or simply don't subset a variable). After you run the command, check the output file with Panoply to check that it has the correct number of variables and only one time step.
 
-system("cdo sub -timmean gfdl_RCM3_Future.nc -timmean gfdl_RCM3_Current.nc GFDL_RCM3_dif.nc")
+system("cdo sub -timmean ../data/gfdl_RCM3_Future.nc -timmean ../data/gfdl_RCM3_Current.nc GFDL_RCM3_dif.nc")
 
   ## 2) Calculate the mean change BY SEASON for all the variables in the GFDL_RCM3 dataset.
   ## The desired output will be a netcdf file with 4 time steps, the mean change for the 4 seasons.
-  ## Hint: you will substitute "timmean" used above for something else.
-  ## You may have to look in the CDO documentation to find the correct command.  Name the output file: gfdl_seasdif.nc
   
-system("cdo sub -yseasmean GFDL_Future.nc -yseasmean GFDL_Current.nc  GFDL_difseas.nc")
-INSERT YOUR CODE HERE******************
+system("cdo sub -yseasmean ../data/GFDL_Future.nc -yseasmean ../data/GFDL_Current.nc  gfdl_seasdif.nc")
 
-
-      ## After you have finished number 2, read the file into R with the following command
-      ## If you named your file something other than gfdl_seasdif.nc, you'll have to edit the file name below
-      ## to match what you called your output above.
-      difseas=as(brick("gfdl_seasdif.nc",varname="tmean",band=1:4),"SpatialGridDataFrame")
+  ## Read the file into R with the following command
+      difseas=brick("gfdl_seasdif.nc",varname="tmean",band=1:4)
       ## that uses the 'brick' command to read in all 4 time steps from tmean as separate raster images
       ## now update the column names
-      colnames(difseas@data)=c("Winter","Spring","Summer","Fall") 
+      names(difseas)=c("Winter","Spring","Summer","Fall") 
 
       ## then plot the change in each of the four seasons
-      spplot(difseas,sp.layout=list("sp.lines",ne),col.regions=rev(heat.colors(20)),scales=list(draw=T))
+      levelplot(difseas,col.regions=rev(heat.colors(20)))+layer(sp.lines(ne))
 
       ## Which season is going to warm the most?
       ## let's look at that another way, as a density plot (like a histogram) with the four seasons.
-      densityplot(~value,groups=variable,data=melt(difseas@data),auto.key=T)
-      ## or the same data again as a boxplot
-      boxplot(value~variable,data=melt(difseas@data),col="grey",ylab="Temperature Change",xlab="Season")
+      densityplot(difseas,auto.key=T)
+      ## or the same data again as a "violin" plot
+      bwplot(difseas,col="grey",ylab="Temperature Change",xlab="Season")
+      ##  Oops, default y limits are a bit too tight, let's fix that...
+      bwplot(difseas,col="grey",ylab="Temperature Change",xlab="Season",scales=list(y=list(lim=c(1,5))))
 
 ##############################################################################
 ### if you have time, make these same plots for precipitation (ptot) by copying the code above and changing the variable name (varname="ptot")...
