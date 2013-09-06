0693211b1bbd14a587c5ba92551c92185a79792e
user <user@user-pc.(none)>
2013-09-06 11:02:13 -0500

    A few edits after testing on LVM

--- RCM/CDO_Process.R
+++ RCM/CDO_Process.R
@@ -1,7 +1,7 @@
 
 ### set the working directory
 setwd("/media/Data/Work/Courses/CyberInfrastructure/ClimateModule/Courses/Day3")
-setwd("C:/Documents and Settings/EEB/Desktop/junk")
+setwd("/home/user/ClimateLabs/RCM")
 
 ## load some libraries that we will need
 library(ncdf)
@@ -34,7 +34,11 @@ system("cdo -V")
 system("cdo vardes gfdl_RCM3_Future.nc")
 
 ## What is the output?  What variables are in that file?
-
+## You can also explore the file with panoply
+## adding the "&" on the end tells linux to start panoply and not wait for it to close/finish
+## But it remains a 'sub' process of R here, so if you close R, the panoply window will also die.
+## See nohup (http://en.wikipedia.org/wiki/Nohup) for ways to spawn an independant process.
+system("/usr/local/PanoplyJ/panoply.sh gfdl_RCM3_Future.nc &")
 
 ## CDO commands generally have the following syntax:  cdo command inputfile outputfile 
 ## and create a new file (outputfile) that is the result of running the command on the 
@@ -98,7 +102,7 @@ scales=list(draw=T))
 ## What patterns do you see at this scale?  An difference between water and land?  Why would this be?
 
 ## The previous plot shows the information over space, but how about a distribution of values:
-hist(mat_dif$values,col="grey",main="Change (future-current) Mean Annual Temperature",xlab="Change (Degrees C)")
+hist(mat_dif@data$Mean.Daily.Temperature,col="grey",main="Change (future-current) Mean Annual Temperature",xlab="Change (Degrees C)")
 
 ######################################################################
 ######################################################################
@@ -123,26 +127,20 @@ system("cdo sub -yseasmean GFDL_Future.nc -yseasmean GFDL_Current.nc  GFDL_difse
 
 ######################################################################
 ######################################################################
-## Ok, now you have to start thinking (a little).  
 ######################################################################
 ## Below are a list of two data products that we want you to develop using the skills you just learned. 
 ## This will require you to run commands similar to the code above to create new summaries of the daily data.
-## But you will have to make small changes to the code above to calculate the products below.
-## I suggest that you copy the commands you need from above (don't edit the above code) and paste them below each question below and edit them there.  
-
-## When you finish, ask Adam to come over and check (or you can email him the two lines of code if you finish after class).
 
 ## 1) Calculate the mean change (future-current) for all the variables in the GFDL_RCM3 dataset (tmin, tmax, tmean, and precipitation) like we did for just temperature above (Hint: do them one by one or simply don't subset a variable). After you run the command, check the output file with Panoply to check that it has the correct number of variables and only one time step.
 
-
-INSERT YOUR CODE HERE******************
+system("cdo sub -timmean gfdl_RCM3_Future.nc -timmean gfdl_RCM3_Current.nc GFDL_RCM3_dif.nc")
 
   ## 2) Calculate the mean change BY SEASON for all the variables in the GFDL_RCM3 dataset.
   ## The desired output will be a netcdf file with 4 time steps, the mean change for the 4 seasons.
   ## Hint: you will substitute "timmean" used above for something else.
   ## You may have to look in the CDO documentation to find the correct command.  Name the output file: gfdl_seasdif.nc
   
-
+system("cdo sub -yseasmean GFDL_Future.nc -yseasmean GFDL_Current.nc  GFDL_difseas.nc")
 INSERT YOUR CODE HERE******************
 
 
