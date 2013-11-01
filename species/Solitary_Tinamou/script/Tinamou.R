763ab77b905cf1c73ff702c4c365a7d43c6d3023
Adam M. Wilson <adammichaelwilson@gmail.com>
2013-10-31 21:49:03 -0400

    Updates from Class

--- species/Solitary_Tinamou/script/Tinamou.R
+++ species/Solitary_Tinamou/script/Tinamou.R
@@ -6,6 +6,7 @@ library (maptools)
 library(sp)
 library(rasterVis)
 library(rgdal)
+library(car)
 
 ## download point data of occurrences from the Global Biodiversity Information Facility (GBIF) dataset 
 gbif_points = gbif('Tinamus' , 'solitarius' , download=T , geo=T)
@@ -17,11 +18,19 @@ ebird = read.table("/home/user/Dropbox/EEB713/week7/Solitary_Tinamou/pointdata/l
 ## import a presence-absence dataset from parks
 parks = read.table ("/home/user/ost4sem/exercise/Solitary_Tinamou/pointdata/Tinamus_solitarius_rowids.asc" , header = TRUE  )
 
+## import expert range
+tin_range=readOGR("/home/user/ost4sem/exercise/Solitary_Tinamou/shp/","iucn_birds_proj")
+tin_range=spTransform(tin_range,CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+
+## Make some pseudo-absences
+pa=as.data.frame(coordinates(spsample(tin_range,70,type="random")))
+
 ## Build a combined dataset (with source, presence/absence, and coordinates)
 points=rbind.data.frame(
   data.frame(src="gbif",obs=1,lat=gbif_points$lat,lon=gbif_points$lon),
   data.frame(src="ebird",obs=1,lat=ebird$LATITUDE,lon=ebird$LONGITUDE),
-  data.frame(src="parks",obs=parks$presence,lat=parks$st_y,lon=parks$st_x)  
+  data.frame(src="parks",obs=parks$presence,lat=parks$st_y,lon=parks$st_x),  
+  data.frame(src="psuedo",obs=0,lat=pa$y,lon=pa$x)  
   )
 ## turn it into a spatial dataframe and define projection
 coordinates(points)=c('lon','lat')
@@ -35,16 +44,14 @@ World  = readShapePoly ("/home/user/ost4sem/exercise/Solitary_Tinamou/shp/world_
 projection(World)="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 
 ## check out the data
-spplot(points,zcol="type")+
+spplot(points,zcol="type",pch=1:5,col.regions=c("red","red","black","red","black"))+
   layer(sp.polygons( World))# ,xlim=c(-80 , -15) , ylim=c(-40,-5) ) ;
 
 ### uh oh, there are some records out in the ocean (typical GBIF!)
 ### There are several ways to handle this, one is to clip by the 'expert' range of the species.
-tin_range=readOGR("/home/user/ost4sem/exercise/Solitary_Tinamou/shp/","iucn_birds_proj")
-tin_range=spTransform(tin_range,CRS(projection(points)))
 
 ## plot it again with the range
-spplot(points,zcol="type",pch=1:4,col.regions=c("red","red","black","red"))+
+spplot(points,zcol="type",pch=1:5,col.regions=c("red","red","black","red"))+
   layer(sp.polygons( World))+
   layer(sp.polygons(tin_range,fill="grey"),under=T)
 
@@ -54,15 +61,26 @@ spplot(points,zcol="type",pch=1:4,col.regions=c("red","red","black","red"))+
 dproj=CRS("+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +a=6371007 +b=6371007 +units=m +no_defs") 
 points$dist=gDistance(spTransform(points,dproj),spTransform(tin_range,dproj),byid=T)[1,]
 ## that adds 'distance' (in meters) from each point to the polygon, check out the values:
-hist(points$dist)
-## so some points are > 2,000km from the range, let's drop any more than 10,000km
+hist(points$dist/1000,xlab="km to expert range")
+## so some points are > 2000km from the range, let's drop any more than 10km
 points=points[points$dist<10000,]
 
 ## plot it again with the range
-spplot(points,zcol="type",pch=1:4,col.regions=c("red","red","black","red"))+
+spplot(points,zcol="type",pch=1:5,col.regions=c("red","red","black","red","black"))+
   layer(sp.polygons( World))+
   layer(sp.polygons(tin_range,fill="grey"),under=T)
 ## that looks better.
+
+
+
+## plot it again adding psuedo-absences
+spplot(points,zcol="type",pch=1:5,col.regions=c("red","red","black","red","black"))+
+  layer(sp.polygons( World))+
+  layer(sp.polygons(tin_range,fill="grey"),under=T)+
+  layer(sp.points(pa,col="purple"))
+
+## that looks better.
+
 
 ################################################################################
 ## Import some evironmental data (Climate & NPP) and align it to a common grid
@@ -72,6 +90,12 @@ names(env)=sub("_34","",names(env))
 names(env)[names(env)=="MOD17A3_Science_NPP_mean_00_12_clip"]="npp"
 ## set missing value in npp
 NAvalue(env[["npp"]])=65535
+
+## get total % forest
+forest=sum(env[[grep("consensus",names(env))]])
+names(forest)="forest"
+## add forest into the env stack
+env=stack(env,forest)
 
 ##  List all available environmental data
 names(env)
@@ -86,18 +110,27 @@ levelplot(env[[var]],col.regions=terrain.colors(20),margin=F)+
 
 ## variable selection is tricky business and we're not going to dwell on it here...  
 ## we'll use the following variables
-vars=c("tmean12","prec12","npp","alt")
-levelplot(env[[vars]])
+vars=c("tmean12","prec12","npp","alt","forest")
+## look at the environmental variability
+plot(env[[vars]])
 
 ## add the environmental data to each point
 pointsd=extract(env[[vars]],points,sp=T)
 
 ## now check out some bivariate plots
-xyplot(npp~alt,groups=obs,data=pointsd@data,auto.key=T)
-xyplot(tmean12~prec12,groups=obs,data=pointsd@data,auto.key=T)
+## feel free to explore other variables
+
+xyplot(npp~alt,groups=type,data=pointsd@data,auto.key=T)+
+  layer(panel.ellipse(x,y,groups=pointsd$type,subscripts=T,level=.68))
+
+xyplot(tmean12~prec12,groups=type,data=pointsd@data,auto.key=T)+
+  layer(panel.ellipse(x,y,groups=pointsd$type,subscripts=T,level=.68))
+
+xyplot(npp~forest,groups=type,data=pointsd@data,auto.key=T)+
+  layer(panel.ellipse(x,y,groups=pointsd$type,subscripts=T,level=.68))
 
 ## Fit a very simple GLM to the data
-m1=glm(obs~tmean12+prec12+npp+alt,data=pointsd@data)
+m1=glm(obs~tmean12+prec12+npp+alt+forest,data=pointsd@data)
 ## Show the model summary
 summary(m1)
 
@@ -110,6 +143,25 @@ levelplot(pred,col.regions=rainbow(20,start=.2,end=.9),margin=F)+
   layer(sp.points(points[points$obs==0,],col="black",cex=2,lwd=4))+ #add absences
   layer(sp.points(points[points$obs==1,],col="red",cex=2,lwd=4))    #add presences
 
+
+## perhaps we should mask the predictions to the expert range
+pred2=mask(pred,tin_range)
+
+levelplot(pred2,col.regions=rainbow(20,start=.2,end=.9),margin=F)+
+  layer(sp.polygons( World))+
+  layer(sp.polygons(tin_range,lwd=2))+
+  layer(sp.points(points[points$obs==0,],col="black",cex=2,lwd=4))+ #add absences
+  layer(sp.points(points[points$obs==1,],col="red",cex=2,lwd=4))    #add presences
+
+## quick accuracy assessment
+predpoints=extract(pred,points,sp=T)
+boxplot(layer~obs,data=predpoints@data,ylab="Estimated Propability of Occurrence",xlab="Observed Presence/Absence")
+
+## to exclude the 'psuedo-absence' points, add this: [pointsd$type!="psuedo",]
+## after pointsd@data
+
+## what if we only used percent forest?
+boxplot(forest~obs,data=pointsd@data,ylab="Percent Forest",xlab="Observed Presence/Absence")
 
 ##########################
 ## the polygons of the parks (and the associate presence/absence status) is available in 
