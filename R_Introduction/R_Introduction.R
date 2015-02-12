3a6c5fdbe673953df4a9d55da3104b312c631bae
Adam M. Wilson <adammichaelwilson@gmail.com>
2015-02-12 11:34:59 -0500

    update readme

--- R_Introduction/R_Introduction.R
+++ R_Introduction/R_Introduction.R
@@ -2,17 +2,13 @@
 #' title: "Introduction to R"
 #' author: "Adam M. Wilson"
 #' date: "February 11, 2015"
-#' output:
+#' output: 
 #'   md_document:
-#'     variant: markdown_github
-#'     toc: true
-#'     toc_depth: 2
+#'     variant: markdown_github  
 #' ---
 #' 
-#' Introduction to R
-#' ====
-#' 
 
+#' 
 #' 
 #' #  Introduction
 #' This script is available:
@@ -20,14 +16,6 @@
 #'   * [SpatialAnalysisTutorials repository](`r repo`)
 #'   * Plain text (.R) with commented text [here](`r paste0(repo,"/",output)`)
 #'  
-## ----message=F,warning=FALSE---------------------------------------------
-library(knitr)
-library(ggplot2)
-
-
-#' 
-#' 
-#' If you don't have the packages above, install them in the package manager or by running `install.packages("ggplot2")`. 
 #' 
 #' ## Vectors
 #' 
@@ -37,6 +25,28 @@ library(ggplot2)
 #' 
 #' 
 #' ## Matrices
+#' 
+#' 
+#' 
+#' ## Loading Packages
+#' 
+#' To load a package, you can simply type `library(package)` where `package` is the name of the package you want to load.  However, this only works for packages that you already have installed on your system.  To install new packages, you can use `install.packages()` or use the package manager. 
+#' 
+#' > R may ask you to choose a CRAN mirror. CRAN is the distributed network of servers that provides access to R's software.  It doesn't really matter which you chose, but closer ones are likely to be faster.  From RStudio, you can select the mirror under Tools→Options.
+#' 
+#' In RStudio, this looks like this:
+#' 
+#' 
+## ----message=F,warning=FALSE---------------------------------------------
+library(ggplot2)
+
+presentation_theme <- theme_grey()+
+  theme(text = element_text(size = 25, colour = "black"))
+
+
+#' 
+#' 
+#' If you don't have the packages above, install them in the package manager or by running `install.packages("ggplot2")`. 
 #' 
 #' 
 #' ## Working with Raster Data
@@ -51,3 +61,6 @@ library(raster)
 #' 
 #' 
 
+#' 
+#' 
+#' 
